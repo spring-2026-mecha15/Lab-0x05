06b7dd9c2ba9c12179ffe4c0a738fb238d22a99f
Max-Schecter <max.schecter@icloud.com>
2026-03-03 11:55:14 -0800

    observer initial changes

--- main.py
+++ main.py
@@ -40,6 +40,9 @@ from task_reflectance import task_reflectance
 gc.collect()
 from task_imu import task_imu
 gc.collect()
+from task_observer import task_observer
+gc.collect()
+
 
 # Inter-task communication and scheduling
 from task_share import Share, Queue, show_all
@@ -140,6 +143,15 @@ imuGx          = Share("f", name="IMU Gyro X")
 imuGy          = Share("f", name="IMU Gyro Y")
 imuGz          = Share("f", name="IMU Gyro Z")
 
+# Observer raw-input shares (unit normalization will happen in observer task)
+motorEffortLeft        = Share("f", name="Left Motor Effort[%]")
+motorEffortRight       = Share("f", name="Right Motor Effort [%]")
+wheelDistLeft          = Share("f", name="Wheel Dist Left [mm]")
+wheelDistRight         = Share("f", name="Wheel Dist Right [mm]")
+#I don't have the code in the imu task setting these shares
+yawDeg            = Share("f", name="Observer Yaw [deg]")
+YawRateDegPerSec  = Share("f", name="Observer Yaw Rate [deg/s]")
+
 
 # ============================================================================
 # TASK INSTANTIATION
@@ -149,12 +161,12 @@ imuGz          = Share("f", name="IMU Gyro Z")
 leftMotorTask = task_motor(
     leftMotor, leftEncoder,
     leftMotorGo, leftMotorKp, leftMotorKi, leftMotorSetPoint,
-    dataValues, timeValues)
+    dataValues, timeValues, wheelDistLeft, motorEffortLeft)
 
 rightMotorTask = task_motor(
     rightMotor, rightEncoder,
     rightMotorGo, rightMotorKp, rightMotorKi, rightMotorSetPoint,
-    dataValues, timeValues)
+    dataValues, timeValues, wheelDistRight, motorEffortRight)
 
 # Create user interface task for parameter adjustment and data collection
 userTask = task_user(
@@ -198,6 +210,11 @@ imuTask = task_imu(
     imuGx, imuGy, imuGz
 )
 
+# Create an Observer instance
+observerTask = task_observer(
+    A_D, B_D, C_D
+)
+
 
 # ============================================================================
 # TASK SCHEDULING SETUP
@@ -218,6 +235,8 @@ task_list.append(Task(lineFollowTask.run, name="Line Follow Task",
                       priority=5, period=15, profile=True))
 task_list.append(Task(imuTask.run, name="IMU Task",
                       priority=10, period=10, profile=True))
+task_list.append(Task(observerTask.run, name="Observer Task",
+                      priority=3, period=10, profile=True))
 
 def garbage_collect():
     while True:
